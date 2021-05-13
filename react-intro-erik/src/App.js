2a2a7ac31669515d81043c9ede4cb2b68f8777c3
erik-engstrom <eekareeka@yahoo.com>
2021-05-13 15:11:34 -0700

    add react intro project

---
+++ react-intro-erik/src/App.js
@@ -0,0 +1,17 @@
+import React, {Component} from 'react'
+import './App.css'
+
+class App extends Component {
+  
+  render() {
+  return ( 
+  <>
+    <h1>Hello Bravo</h1>
+    <h2>Smaller Hello Bravo</h2>
+    <h3>Even smaller Hello Bravo</h3>
+    <h4>One of the smallest Hello Bravo</h4>
+  </>
+    )
+  }
+}
+export default App
