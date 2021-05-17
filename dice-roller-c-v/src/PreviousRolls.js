f518a2a0481d3e3d8cfec84e57b9cc87f7c35d5f
LEARN Academy <learnacademy@LEARNs-MacBook-Air.local>
2021-05-17 17:47:23 -0500

    initial commit

---
+++ dice-roller-c-v/src/PreviousRolls.js
@@ -0,0 +1,11 @@
+import React, { Component } from 'react'
+
+export default class PreviousRolls extends Component{
+    render() {
+    return (
+        <div>
+            <h1>{this.props.prop}</h1>
+            </div>
+    );
+}
+}
