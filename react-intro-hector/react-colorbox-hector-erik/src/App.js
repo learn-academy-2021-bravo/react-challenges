7c010ebd941842657cfced08a53c43d734c491a9
hexflo <hexflores87@yahoo.com>
2021-05-17 14:18:10 -0700

    left off on css

--- react-intro-hector/react-colorbox-hector-erik/src/App.js
+++ react-intro-hector/react-colorbox-hector-erik/src/App.js
@@ -1,39 +1,31 @@
-import logo from './logo.svg';
 import './App.css';
 import React, { Component } from 'react'
 
-class Box extends Component {
-  constrcutor(props){
+class App extends Component {
+  constructor(props){
     super(props)
     this.state = {
 
+   
+    }
+    handleChange = () => {
+      
     }
   }
   render() {
+    return(
+      <>
+      <div className="box" onClick={ this.handleChange}></div>
+      <button onClick = { this.handleChange }>Click Me!</button>
+      
 
+ </>
+
+    )
   }
 }
 
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+
 
 export default App;
