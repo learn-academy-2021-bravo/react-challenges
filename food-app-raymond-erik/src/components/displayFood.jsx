5b3da8bd004e430b818c2c33e7efaf4f460ec4eb
erik-engstrom <eekareeka@yahoo.com>
2021-05-19 13:00:13 -0700

    first push for food app

---
+++ food-app-raymond-erik/src/components/displayFood.jsx
@@ -0,0 +1,16 @@
+import React, {Component} from "react"
+
+class DisplayFood extends Component{
+render(){
+  console.log(this.props.foodList);
+  console.log(this.props.price);
+  return(
+    <>
+    
+    </>
+
+  )
+}
+}
+
+export default DisplayFood
