7e107f2562c371a6523aec6eed74639edec127d0
guillermogb88 <guillermo.garcia08@yahoo.com>
2021-05-24 16:03:04 -0700

    inputs displaying on webpage

---
+++ react-forms-guillermo-shaz-hex/src/App.js
@@ -0,0 +1,14 @@
+import logo from './logo.svg';
+import './App.css';
+import MadLibs from './component/MadLibs'
+
+function App() {
+  return (
+    <div className="App">
+    <MadLibs/>
+    
+    </div>
+  );
+}
+
+export default App;
---
+++ react-forms-guillermo-shaz-hex/src/component/MadLibs.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react'
+import Forms from './Forms'
+
+export default class MadLibs extends Component {
+    render() {
+        return (
+            <div>
+                <h1>MadLibs</h1>
+                <Forms/>
+            </div>
+        )
+    }
+}
