8b3632e073590d055f877415b11622ada79e9503
guillermogb88 <guillermo.garcia08@yahoo.com>
2021-05-24 21:47:59 -0700

    functionality complete styling missing

--- react-forms-guillermo-shaz-hex/src/component/MadLibs.js
+++ react-forms-guillermo-shaz-hex/src/component/MadLibs.js
@@ -17,8 +17,8 @@ export default class MadLibs extends Component {
             <div>
                 <h1>MadLibs</h1>
                 <Forms handleSubmitClick = {this.handleSubmitClick}/>
-                {this.state.madLibs && (<div><p>Once upon a time there lived a very sad banana named {this.state.madLibs.boysName}. A boy's name lived in a A type of packaging in Fruitsville. He was sad because he had no friends, but then he thought, "How am I going to get friends if I don't go looking for some? I am going to go out and look for some friends."</p>
-                <p>So A boy's name put on his A fashion accessory to impress people and went out to A place. He was walking around A place when he met a nice lady banana called A girl's name. She said, "Hello, my name is A girl's name. Do you want to go to A park, and go on the A park attraction? A boy's name said, "OK, If you will be my friend." "Yes!" She replied. So A boy's name and A girl's name became good friends and A boy's name wasn't sad anymore.</p>
+                {this.state.madLibs && (<div><p>Once upon a time there lived a very sad banana named {this.state.madLibs.boysName}. {this.state.madLibs.boysName} lived in a {this.state.madLibs.typeOfPackaging} in Fruitsville. He was sad because he had no friends, but then he thought, "How am I going to get friends if I don't go looking for some? I am going to go out and look for some friends."</p>
+                <p>So {this.state.madLibs.boysName} put on his {this.state.madLibs.fashionAcessory} to impress people and went out to {this.state.madLibs.place}. He was walking around the {this.state.madLibs.place} when he met a nice lady banana called {this.state.madLibs.girlsName}. She said, "Hello, my name is {this.state.madLibs.girlsName} . Do you want to go to {this.state.madLibs.park} and go on the {this.state.madLibs.park} attractions? {this.state.madLibs.boysName}, "OK, If you will be my friend." "Yes!" She replied. {this.state.madLibs.boysName} and {this.state.madLibs.park} became good friends and {this.state.madLibs.boysName} wasn't sad anymore.</p>
                 <p>~The End~</p></div>)}
                 
             </div>
